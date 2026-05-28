7b3f6402460b3dd119801ee545039cc580c693ac
Arsridi <159222444+Arsridi@users.noreply.github.com>
2026-05-28 23:52:23 +0800

    Initial commit

---
+++ src/main/java/net/arsridi/enchantedrunes/EnchantedRunesClient.java
@@ -0,0 +1,10 @@
+package net.arsridi.enchantedrunes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnchantedRunesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
